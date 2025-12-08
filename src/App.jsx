2f42ed07d78c8d9c0b15a7eb60c694fb516dbf3b
Aditya Akundi <aakundi@aldar.com>
2025-12-08 15:41:37 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Hello AWS Deployment!</h1>
+      <p>This is my first React app deployed on S3.</p>
+    </div>
+  );
+}
+
+export default App;
